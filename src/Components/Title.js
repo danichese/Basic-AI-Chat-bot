cd97ee964afccb0a3e227038fe5cede7df4e95f4
Daniel Cheeseman <daniel.cheeseman6@gmail.com>
2023-03-21 20:40:02 +0000

    Added title

---
+++ src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as Title } from 'C:/Users/danie/Documents/FIGMA/Group.svg'
+
+function TitleComponent() {
+    return(
+        <div>
+      <Title />
+    </div>
+    );
+}
+
+export default TitleComponent;
