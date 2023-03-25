f481d9ef009caebf3aafee909542f588974f41ea
Daniel Cheeseman <daniel.cheeseman6@gmail.com>
2023-03-25 16:19:05 +0000

    Backend added - need to uninstall and reinstall node and mongo to project dir

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import TitleComponent from './Components/Title';
+import ChatboxComponent from './Components/Chatbox';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Title />
+        <TitleComponent />
+        <ChatboxComponent/>
       </header>
     </div>
   );
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReactComponent as Title } from 'C:/Users/danie/Documents/FIGMA/Group.svg'
+import { ReactComponent as Title } from './Title.svg'
 
 function TitleComponent() {
     return(
